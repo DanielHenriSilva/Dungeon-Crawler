f07a62736d023cd61ade2104bad6601d99f4e2e7
Daniel <daniel25070104@aluno.cesupa.br>
2025-05-09 10:40:30 -0300

    menu atualizado, mas falta implementar a função de jogar, sair e creditos

--- Dungeon-Crawler.c
+++ Dungeon-Crawler.c
@@ -6,6 +6,57 @@
 #define coluna 10
 int px = 1, py = 1;
 
+void menu (void)
+{
+   
+    int i = 0;
+    while (i < 5)
+    {
+        printf("\n");
+        i++;
+    }
+    
+    printf("\033[1m");
+    printf("\033[3m");
+    printf("%*s\n", 83, "DUNGEONS\n");
+    printf("%*s\n", 80, "Of\n");
+    printf("\033[0m");
+    printf("\033[1;31m");
+    
+
+    i = 0;
+    while (i < 2)
+    {
+        printf("\n");
+        i++;
+    }
+
+    printf("%*s\n", 119, "####  ####      ##  ######  ######   ########    ####      ##  ##      ##   ######");
+    printf("%*s\n", 114, " ##   ## ##     ##  ##      ##       ##     ##   ## ##     ##  ##      ##  ##");
+    printf("%*s\n", 114, " ##   ##  ##    ##  ##      ##       ##     ##   ##  ##    ##  ##      ##  ##");
+    printf("%*s\n", 119, " ##   ##   ##   ##  ######  ######   ########    ##   ##   ##  ##      ##   ######");
+    printf("%*s\n", 120, " ##   ##    ##  ##  ##      ##       ##    ##    ##    ##  ##  ##      ##        ##");
+    printf("%*s\n", 120, " ##   ##     ## ##  ##      ##       ##     ##   ##     ## ##  ##      ##        ##");
+    printf("%*s\n", 119, "####  ##      ####  ##      ######   ##      ##  ##      ####  ##########   ######");
+    printf("\033[0m");
+
+    i = 0;
+    while (i < 3)
+    {
+        printf("\n");
+        i++;
+    }
+    
+    printf("\033[1m");
+    printf("\033[3m");
+    printf("%*s\n", 82, "Play\n");
+    printf("%*s\n", 82, "Help\n");
+    printf("%*s\n", 82, "Exit\n");
+    printf("\033[0m");
+    printf("\033[1;31m");
+    
+}
+
 void mapa1 (char mapa[linha][coluna])
 {
     int i = 0, j;
--- teste3.c
+++ teste3.c
@@ -1,18 +1,10 @@
 #include <stdio.h>
+#include <conio.h>
 
 int main() {
-    // Códigos ANSI para cores e estilos
-    printf("\033[1;31m"); // Vermelho brilhante
-    printf("Dungeons Of - Estilo Vermelho\n");
-    printf("\033[0m"); // Resetar para padrão
-    
-    printf("\033[1;34;47m"); // Azul brilhante com fundo branco
-    printf("Dungeons Of - Estilo Azul com Fundo\n");
-    printf("\033[0m");
-    
-    printf("\033[3;35m"); // Itálico magenta
-    printf("Dungeons Of - Estilo Itálico\n");
-    printf("\033[0m");
+    int input;
+    input = getch();
+    printf("%d", input);
     
     return 0;
 }
